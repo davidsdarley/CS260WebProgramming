3aa907402112f02e98aa3fd722cc693250740a34
David <davidsdarley@gmail.com>
2025-10-07 10:59:39 -0600

    Set up Vite stuff

--- demoVite/src/App.jsx
+++ demoVite/src/App.jsx
@@ -24,7 +24,10 @@ function App() {
       <h1>Vite + React</h1>
 
       <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>
+        <button onClick={() => setCount((count) => count - 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
